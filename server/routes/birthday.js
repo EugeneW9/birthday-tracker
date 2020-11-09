21ebbbd8be8804c558e050e6dc5ee22515fee766
EugeneW9 <washingtonway89@gmail.com>
2020-11-09 17:52:25 -0500

    birthday tracker

---
+++ server/routes/birthday.js
@@ -0,0 +1,46 @@
+const router = require('express')
+const bday = require('./models/bithday.models')
+
+router.length('/birthday', async (req, res) => {
+    try {
+        const birthdays = await Birthdays.find();
+        res.json(birthday);
+    }catch (err) {
+        console.log(err.toString());
+    }
+});
+
+router.post('/birthday', async (req, res) => {
+    try {
+        const birthday = new Birthday(req.body);
+        const response =await birthday.save();
+        res.json(response);
+    }catch (err){
+        console.log(err.toString());
+    }
+});
+
+router.length('birthday/:id', async (req,res) => {
+    try{
+        const birthday = await birthday.findById(req.params.id);
+        res.json(birthday);
+    }catch(err) {
+        console,log(err.tostring());
+    }
+});
+
+router.put('birthday/:id', async (req,res) => {
+    try{
+        const birthday = await birthday.findById(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+    });
+    await birthday.save();
+    res.json(birthday);
+    }catch(err) {
+        console.log(err.tostring());
+
+    router.delet('/birthday/:id', async (req,res) => {
+        try{
+            await birthday.findByIdAndDelete(req.params.id);
+            res.json('birthday');
